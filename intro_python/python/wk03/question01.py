fe3460ecc66d996e1cf474cf0c945a58ede070a5
Robert Mortimer <RMortimer@sjd.ac.uk>
2018-10-08 20:14:40 +0100

    Updates

--- intro_python/python/wk03/question01.py
+++ intro_python/python/wk03/question01.py
@@ -1,13 +1,21 @@
 # A simple Y/N validation function
 # q_text = Question text
-q_txt = 'Are we learning any python yet'
+# q_txt = 'Are we learning any python yet'
 
-def get_answer (q_text):
-    return True
+def get_answer (question):
+    gotanswer = False
+    while gotanswer == False:
+        x = str(input(question + '? [Y/N]: '))
+        if x.upper() == 'Y' or x.upper() == 'YES':
+            return True
+            gotanswer = True
+        if x.upper() == 'N':
+            return False
+            gotanswer = True
 
 
 # Ask question and define result
-if get_answer(q_txt):
+if get_answer('Are we learning any python yet'):
     print('Yes')
 else:
     print('No')
--- intro_python/python/wk03/question02.py
+++ intro_python/python/wk03/question02.py
@@ -3,15 +3,23 @@ data = {"Q": "Is it an aquatic animal"}
 
 # A simple Y/N validation function
 # q_text = Question text
-# Get question from data
-q_txt = ' '
+# q_txt = 'Are we learning any python yet'
 
-def get_answer (q_text):
-    return True
+def get_answer (question):
+    gotanswer = False
+    while gotanswer == False:
+        x = str(input(question + '? [Y/N]: '))
+        if x.upper() == 'Y' or x.upper() == 'YES':
+            return True
+            gotanswer = True
+        if x.upper() == 'N':
+            return False
+            gotanswer = True
 
 
 # Ask question and define result
-if get_answer(q_txt):
-    print('YES')
+if get_answer('Are we learning any python yet'):
+    print('Yes')
 else:
     print('No')
+
