dda44f3e93bbce07676f06d3ef4bda953159cb18
Robert Mortimer <RMortimer@sjd.ac.uk>
2018-10-22 20:36:26 +0100

    Roman Numbers

--- intro_python/python/wk05/roman.py
+++ intro_python/python/wk05/roman.py
@@ -25,17 +25,45 @@ arabic_nbr = {
     50   : 'L',
     100  : 'C',
     500  : 'D',
-    1000 : 'M'
+    1000 : 'M',
+
 }
 
 x = int(input('Enter your number : '))
 
-# Loop through our our numbers in order
-for k in arabic_nbr:
-  print(arabic_nbr[k])
+if x < 4000:
+  roman_number = ''
+  while x > 0:
+    # Loop through our our numbers in order
+    bigest_int = 0
+    smallest_count = 0
+    for k in arabic_nbr:
+      print(str(k) + ' : ' + arabic_nbr[k])
+      y = divmod(x, k)
+      if (y[0] != 0) and (k > bigest_int):
+        print(str(k) + ' Devides ' + str(y[0]) + ' Times' )
+        bigest_int = k
+        smallest_count = y[0]
+    while smallest_count > 0:
+      roman_number = roman_number + arabic_nbr[bigest_int]
+      smallest_count += -1
+      x = x - bigest_int
 
-for k in roman_nbr:
-  print(roman_nbr[k])
+
+
+  print('Start with ' + str(bigest_int) )
+  print('Number ' + roman_number )
+  
+
+
+
+
+  # for k in roman_nbr:
+  #   print(roman_nbr[k])
+
+else:
+  print('We only do numbers under 4000')
+
 
 # Find exact division with remainder
 #x = divmod(1420, 500)
